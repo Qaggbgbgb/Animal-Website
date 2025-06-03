3fa1fb188e4e87f43de354828f0a1186b6cf8187
Tianrui Zhang <t_zhang5@uncg.edu>
2025-06-02 21:04:31 -0400

    initialize repository

---
+++ src/main/java/TZ/demo/AnimalWebsiteApplication.java
@@ -0,0 +1,13 @@
+package TZ.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnimalWebsiteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AnimalWebsiteApplication.class, args);
+	}
+
+}
---
+++ src/test/java/TZ/demo/AnimalWebsiteApplicationTests.java
@@ -0,0 +1,13 @@
+package TZ.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnimalWebsiteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
